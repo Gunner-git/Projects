86f194208817fb30f9c962a768a8cefb1e0b7840
Gunner-git <gurnoor2005ronaldo.com>
2022-08-09 16:08:44 +0530

    Ignore the profanity please

---
+++ wordle_cli.py
@@ -0,0 +1,40 @@
+import random
+
+wordArray = ["cocks", "balls","vagin","bewbs"]
+currentWord = wordArray[int(random.randint(0,(len(wordArray)-1)))]
+userGuessArray = []
+userGuessLetterArray = []
+starMatrix = [
+            ["*", "*", "*", "*", "*"],
+            ["*", "*", "*", "*", "*"],
+            ["*", "*", "*", "*", "*"],
+            ["*", "*", "*", "*", "*"],
+            ["*", "*", "*", "*", "*"]
+            ]
+j = 0
+
+while len(userGuessArray)<5:
+    if len(userGuessArray) == 0:
+        for i in starMatrix:
+            print(i)
+    else:
+        userGuessLetterArray = []
+        for letter in userGuessArray[j]:
+            userGuessLetterArray.append(letter)
+        starMatrix[j] = userGuessLetterArray
+        j += 1
+        for i in starMatrix:
+            print(i)
+
+    userInput = input("Enter a word: ")
+    if len(userInput) == 5:
+        if userInput == currentWord:
+            print("Congratulations! You won in " + str(len(userGuessArray)) + " tries!")
+            break
+        else:
+            userGuessArray.append(userInput)
+    else:
+        print("Enter a 5 letter word")
+
+if len(userGuessArray)>=5:
+    print("Oh no you lost. The word was: " + currentWord)
